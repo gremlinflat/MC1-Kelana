96c681af9c950caf55665e9f9ed36596f691970a
Rizky Maulana <mrizky9601@gmail.com>
2022-04-09 15:22:41 +0700

    Tambah Collection View untuk Recommended

--- kelana/Home/HomeViewController.swift
+++ kelana/Home/HomeViewController.swift
@@ -7,35 +7,68 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UISearchResultsUpdating {
-
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text else{
-             return
-        }
-    }
-    
-
+class HomeViewController: UIViewController {
+    @IBOutlet weak var detailCollectionView: UICollectionView!
     let searchController = UISearchController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBar.prefersLargeTitles = true
         title = "Denpasar, Bali"
-        navigationItem.searchController = searchController
-        searchController.searchResultsUpdater = self
+        setupCollectionView()
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    private func setupCollectionView(){
+        detailCollectionView.delegate = self
+        detailCollectionView.dataSource = self
+        detailCollectionView.collectionViewLayout = createCollectionViewLayout()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        detailCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
-    */
+}
+extension HomeViewController:UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        print(indexPath)
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        cell.layer.cornerRadius = 8
+        return cell
+    }
+}
 
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    private func createCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout {
+            (section, _) -> NSCollectionLayoutSection? in
+            if section == 0 {
+                let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension:.fractionalWidth(1), heightDimension: .fractionalHeight(1)
+                    )
+                )
+                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 8)
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(234)), subitem: item, count: 2)
+                
+                group.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0)
+                
+                let section = NSCollectionLayoutSection(group: group)
+                
+                section.orthogonalScrollingBehavior = .continuous
+                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+                
+                return section
+            }
+            return nil
+        }
+    }
+    
 }
