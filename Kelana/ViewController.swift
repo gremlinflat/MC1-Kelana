ec8263b58b6c65aafd2585d194e0c3dfa179d8d6
Gremlinflat <fahri.novaldi91100@gmail.com>
2022-04-11 16:38:54 +0700

    chore

---
+++ Kelana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kelana
+//
+//  Created by Fahri Novaldi on 07/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+ 
