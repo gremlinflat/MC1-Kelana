7edfd7820bbcfb35228778e773e8f74bb55f5e51
billaza <nabillazarine@gmail.com>
2022-04-13 14:41:37 +0700

    commit kiki

---
+++ Kelana/Home/EventTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EventTableViewCell.swift
+//  kelana
+//
+//  Created by Rizky Maulana on 12/04/22.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var locationImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+}
---
+++ Kelana/Home/RecommendedCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  RecommendedCollectionViewCell.swift
+//  kelana
+//
+//  Created by Rizky Maulana on 12/04/22.
+//
+
+import UIKit
+
+class RecommendedCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var locationImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+}
